32c1fa9fc4c6f4b0d0421764505db173c3a6ade6
Wingwoid <148968236+Wingwoid@users.noreply.github.com>
2025-04-12 23:20:03 +0700

    Update dice.js

--- js/dice.js
+++ js/dice.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const diceBtn = document.querySelector('.dice-button');
+  if (diceBtn) {
+    diceBtn.addEventListener('click', () => {
+      diceBtn.classList.add('dice-rolling');
+      setTimeout(() => {
+        diceBtn.classList.remove('dice-rolling');
+      }, 500);
+    });
+  }
+});
 
